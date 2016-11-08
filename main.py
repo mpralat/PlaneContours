9a471243780a423810922d9c5e2dc886a3d1dbd5
Marta Prałat <pralatmarta@gmail.com>
2016-11-08 19:12:22 +0100

    Merge branch 'master' of /home/marta/PycharmProjects/PlaneContours with conflicts.

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ import argparse
 import sys
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+
 
 colors = [
-    (204, 204, 0),
-    (255, 0, 0),
-    (255, 255, 0),
-    (0, 255, 255),
-    (255, 0, 255),
-    (255, 51, 153)
+    (204, 0, 0),
+    (204,204,0),
+    (0,204,0),
+    (102,0,204),
+    (204,0,204),
+    (255,128,0),
+    (255,255,0),
+    (0, 204, 204),
+    (0,255,0),
+    (51,255,255),
+    (255,102,178),
+    (153,255,153),
+    (255,153,153)
 ]
 color_idx = 0
 
@@ -37,15 +44,6 @@ def parse(arguments):
     return parser.parse_args()
 
 
-def convert_to_black_white(pixel):
-    r, g, b = pixel
-    brightness = np.math.sqrt(0.299 * (r ** 2) + 0.587 * (g ** 2) + 0.114 * (b ** 2))
-    if brightness < 0.5:
-        return (0, 0, 0)
-    else:
-        return (255, 255, 255)
-
-
 def auto_canny(image, sigma=0.33):
     # Compute the median of the single channel pixel intensities
     v = np.median(image)
@@ -66,6 +64,7 @@ def transform(img_path):
     img_color = cv2.imread(img_path)
     cv2.imwrite('test1_orig.jpg', img_grayscale)
 
+    cv2.imwrite('test_gamma_correction.jpg', img_grayscale)
     # Here we need to apply different functions to obtain a good base for getting the contours
     # Apply morphological transformation - opening (erosion and then dilation) - reduces noise
     img_grayscale = cv2.morphologyEx(img_grayscale, cv2.MORPH_OPEN, kernel=np.ones((5, 5), np.uint8))
@@ -74,21 +73,9 @@ def transform(img_path):
     # Apply histogram equalisation based on the cumulative distribution of intensity with limited contrast
     clahe = cv2.createCLAHE(clipLimit=0.35, tileGridSize=(5, 5))
     img_grayscale = clahe.apply(img_grayscale)
-    # cv2.imwrite('test3_clahe.jpg', img_grayscale)
+    cv2.imwrite('test3_clahe.jpg', img_grayscale)
 
-    # Adaptive thresholding
-    # img_grayscale = cv2.equalizeHist(img_grayscale)
-    # cv2.adaptiveThreshold(img_grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 5)
-    # cv2.imwrite('test3_adaptiveThreshold.jpg', img_grayscale)
 
-    # Gaussian blur
-    # img_grayscale = cv2.GaussianBlur(img_grayscale, (0, 0), 0.25)
-    # img_grayscale = cv2.addWeighted(img_grayscale, 1.5, blurred_image, -0.5, 0)
-    cv2.imwrite('test4_sharpen.jpg', img_grayscale)
-
-    # img_grayscale = cv2.morphologyEx(img_grayscale, cv2.MORPH_CLOSE, kernel=np.ones((5, 5), np.uint8))
-    # cv2.imwrite('test5_morph2.jpg', img_grayscale)
-    #img_grayscale = cv2.Canny(img_grayscale, 104, 170)
     img_grayscale = auto_canny(img_grayscale, 0.560)
     cv2.imwrite('test6_canny.jpg', img_grayscale)
     # Apply dilation to merge neighbouring contours
@@ -107,8 +94,6 @@ def transform(img_path):
             cv2.circle(img_color, center=(centroid_x, centroid_y), radius=2, color=(255, 255, 255))
             cv2.drawContours(img_color, contours, idx, colors[color_idx], 3)
             color_idx = (color_idx + 1) % len(colors)
-    # ctn = sorted(contours, key=cv2.contourArea, reverse=True)[:20]
-    # cv2.drawContours(img_color, contours, -1, (255,0,0), 3)
 
     return img_color
 
@@ -119,13 +104,8 @@ if __name__ == "__main__":
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
-    fig = plt.figure()
-
     print(args.output)
-    for idx, img in enumerate(glob.glob(os.path.join(args.input, "*.jpg"))):
-        print(os.path.join(args.output, os.path.basename(img)))
-        a = fig.add_subplot(6, 3, idx+1)
-        # cv2.imwrite(os.path.join(args.output, os.path.basename(img)), transform(img))
-        imgplot = plt.imshow(transform(img))
 
-    plt.show()
+    for img in glob.glob(os.path.join(args.input, "*.jpg")):
+        print(os.path.join(args.output, os.path.basename(img)))
+        cv2.imwrite(os.path.join(args.output, os.path.basename(img)), transform(img))
